4947656ec5a60503cc92e68fc30cfc863c11b0de
bottlecaps4 <brianoh8@gmail.com>
2018-11-07 16:02:29 +0000

    Create Board and Counter Components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerOneLife: 20,
+      playerTwoLife: 20
+    };
+
+    this.incrementLife = this.incrementLife.bind(this);
+    this.decrementLife = this.decrementLife.bind(this);
+    this.resetAllLife = this.resetAllLife.bind(this);
+  }
+
+  incrementLife(life) {
+    this.setState({
+      [life]: this.state[life] + 1
+    });
+  }
+
+  decrementLife(life) {
+    this.setState({
+      [life]: this.state[life] - 1
+    });
+  }
+
+  resetAllLife() {
+    this.setState({
+      playerOneLife: 20,
+      playerTwoLife: 20
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Counter
+          initialLife={this.state.playerOneLife}
+          onPlusClick={() => this.incrementLife("playerOneLife")}
+          onMinusClick={() => this.decrementLife("playerOneLife")}
+        />
+        <Counter
+          initialLife={this.state.playerTwoLife}
+          onPlusClick={() => this.incrementLife("playerTwoLife")}
+          onMinusClick={() => this.decrementLife("playerTwoLife")}
+        />
+        <button onClick={this.resetAllLife}>Reset both Counters</button>
       </div>
     );
   }
 }
+class Counter extends React.Component { 
+  
+  render() {
+    return (
+      <div>
+        <div className="LifeTotal">
+          <button className="AddLife" onClick={this.props.onPlusClick}>+</button>
+            {this.props.initialLife}
+          <button onClick={this.props.onMinusClick}>-</button>
+        </div>
+        
+      </div>
+    )
+  }
+} 
 
-export default App;
+export default Board;
